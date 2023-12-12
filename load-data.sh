551252b5e10cde481e93112cb6af0c8818875d99
Erik Westlund <ewestlund@jhu.edu>
2023-12-12 16:28:22 -0500

    fix names

--- load-data.sh
+++ load-data.sh
@@ -4,19 +4,19 @@ mkdir -p schema
 
 # Fetch and load the schema
 cp CommonDataModel/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_ddl.sql schema/ddl.sql
-sed -i "s/@cdmDatabaseSchema/public/g" schema/ddl.sql
+sed -i "s/@cdmDatabaseSchema/cdm/g" schema/ddl.sql
 sudo -u postgres psql -f schema/ddl.sql
 
 cp CommonDataModel/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_primary_keys.sql schema/keys.sql
-sed -i "s/@cdmDatabaseSchema/public/g" schema/keys.sql
+sed -i "s/@cdmDatabaseSchema/cdm/g" schema/keys.sql
 sudo -u postgres psql -f schema/keys.sql
 
 cp CommonDataModel/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_constraints.sql schema/constraints.sql
-sed -i "s/@cdmDatabaseSchema/public/g" schema/constraints.sql
+sed -i "s/@cdmDatabaseSchema/cdm/g" schema/constraints.sql
 sudo -u postgres psql -f schema/constraints.sql
 
 cp CommonDataModel/inst/ddl/5.4/postgresql/OMOPCDM_postgresql_5.4_indices.sql schema/indices.sql
-sed -i "s/@cdmDatabaseSchema/public/g" schema/indices.sql
+sed -i "s/@cdmDatabaseSchema/cdm/g" schema/indices.sql
 sudo -u postgres psql -f schema/indices.sql
 
 # Now load the Eunomia data, which we have copied into CSV files, following
