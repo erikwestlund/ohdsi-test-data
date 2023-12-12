c161438c10922dbc09770efbd86044314f4d3e8e
Erik Westlund <ewestlund@jhu.edu>
2023-12-12 16:44:49 -0500

    fix gitignore

--- fetch-data.sh
+++ fetch-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/OHDSI/CommonDataModel
+git clone https://github.com/OHDSI/CommonDataModel remote/CommonDataModel
 
-git clone https://github.com/OHDSI/Eunomia
-tar -xvf Eunomia/inst/sqlite/cdm.tar.xz
+git clone https://github.com/OHDSI/Eunomia remote/Eunomia
+tar -xvf remote/Eunomia/inst/sqlite/cdm.tar.xz
