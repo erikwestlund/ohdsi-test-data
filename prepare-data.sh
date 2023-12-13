bb69386c6c28ec0c6c076dd8489d98a4534dba69
Erik Westlund <ewestlund@jhu.edu>
2023-12-13 11:15:09 -0500

    preapre data path bug

--- prepare-data.sh
+++ prepare-data.sh
@@ -10,14 +10,14 @@ tar -xvf remote/Eunomia/inst/sqlite/cdm.tar.xz
 # Get and prepare the schema for CDM 5.3
 mkdir -p sql/schema
 
-cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_ddl.sql data/schema/ddl.sql
+cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_ddl.sql sql/schema/ddl.sql
 sed -i "s/@cdmDatabaseSchema/cdm/g" sql/schema/ddl.sql
 
-cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_primary_keys.sql data/schema/keys.sql
+cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_primary_keys.sql sql/schema/keys.sql
 sed -i "s/@cdmDatabaseSchema/cdm/g" sql/schema/keys.sql
 
-cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_constraints.sql data/schema/constraints.sql
+cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_constraints.sql sql/schema/constraints.sql
 sed -i "s/@cdmDatabaseSchema/cdm/g" sql/schema/constraints.sql
 
-cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_indices.sql data/schema/indices.sql
+cp remote/CommonDataModel/inst/ddl/5.3/postgresql/OMOPCDM_postgresql_5.3_indices.sql sql/schema/indices.sql
 sed -i "s/@cdmDatabaseSchema/cdm/g" sql/schema/indices.sql
