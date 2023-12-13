26406bbc1671c64c20e4586437d2f81a5a7988aa
Erik Westlund <ewestlund@jhu.edu>
2023-12-13 11:16:53 -0500

    filename error

--- load-data.sh
+++ load-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sudo -u postgres psql -f sql/create-ohdsi-role.sql
-sudo -u postgres psql -f sql/set-ohdsi-role-permissions.sql
+sudo -u postgres psql - sql/set-ohdsi-role-permissions.sh
 sudo -u postgres psql -f sql/schema/ddl.sql;
 sudo -u postgres psql -f sql/schema/keys.sql;
 sudo -u postgres psql -f sql/schema/constraints.sql;
