0a8c52bb0fedcda0ba03e0b40677423547e879f7
Erik Westlund <ewestlund@jhu.edu>
2023-12-13 11:31:50 -0500

    ugh fix up

--- sql/set-ohdsi-role-permissions.sql
+++ sql/set-ohdsi-role-permissions.sql
@@ -1,6 +1,8 @@
-GRANT CONNECT ON DATABASE postgres TO ohdsi, postgres;
-GRANT USAGE ON SCHEMA cdm, scratch TO ohdsi, postgres;
-GRANT ALL ON SCHEMA cdm, scratch TO ohdsi, postgres;
+GRANT CONNECT ON DATABASE postgres TO ohdsi;
+GRANT USAGE ON SCHEMA cdm, scratch TO ohdsi;
+GRANT ALL ON SCHEMA cdm, scratch TO ohdsi;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA cdm, scratch TO ohdsi;
+
 SET search_path TO cdm, public;
 ALTER ROLE ohdsi SET search_path TO cdm, public;
 ALTER DEFAULT PRIVILEGES FOR ROLE ohdsi IN SCHEMA cdm GRANT ALL ON TABLES TO ohdsi;
