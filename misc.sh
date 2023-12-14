097bcc0007a58301155ddb5388b44cd893f9523b
Erik Westlund <ewestlund@jhu.edu>
2023-12-14 15:06:15 -0500

    misc.sh was empty

--- misc.sh
+++ misc.sh
@@ -1,1 +1,8 @@
-misc.sh
+
+# Quickly generate CSVs of every table in the SQLite Eunomia database.
+mkdir -p data/eunomia/raw
+TABLES=$(sqlite3 cdm.sqlite .tables)
+
+for table in `echo $TABLES`; do
+  sqlite3 -header -csv cdm.sqlite "select * from $table;" > data/eunomia/raw/$table.csv
+done
