657fd92b8d7953f319573686e0811ff358457354
Erik Westlund <ewestlund@jhu.edu>
2023-12-14 06:10:55 -0500

    finish prep

--- prepare-data.sh
+++ prepare-data.sh
@@ -157,40 +157,141 @@ from DRUG_EXPOSURE;" > data/eunomia/DRUG_EXPOSURE.csv
 
 # MEASUREMENT
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  cast(MEASUREMENT_ID as integer) as MEASUREMENT_ID,
+  cast(PERSON_ID as integer) as PERSON_ID,
+  cast(MEASUREMENT_CONCEPT_ID as integer) as MEASUREMENT_CONCEPT_ID,
+  cast(MEASUREMENT_DATE as integer) as MEASUREMENT_DATE,
+  cast(MEASUREMENT_DATETIME as integer) as MEASUREMENT_DATETIME,
+  cast(MEASUREMENT_TIME as integer) as MEASUREMENT_TIME,
+  cast(MEASUREMENT_TYPE_CONCEPT_ID as integer) as MEASUREMENT_TYPE_CONCEPT_ID,
+  cast(OPERATOR_CONCEPT_ID as integer) as OPERATOR_CONCEPT_ID,
+  VALUE_AS_NUMBER,
+  cast(VALUE_AS_CONCEPT_ID as integer) as VALUE_AS_CONCEPT_ID,
+  cast(UNIT_CONCEPT_ID as integer) as UNIT_CONCEPT_ID,
+  RANGE_LOW,
+  RANGE_HIGH,
+  cast(PROVIDER_ID as integer) as PROVIDER_ID,
+  cast(VISIT_OCCURRENCE_ID as integer) as VISIT_OCCURRENCE_ID,
+  cast(VISIT_DETAIL_ID as integer) as VISIT_DETAIL_ID,
+  MEASUREMENT_SOURCE_VALUE,
+  cast(MEASUREMENT_SOURCE_CONCEPT_ID as integer) as MEASUREMENT_SOURCE_CONCEPT_ID,
+  UNIT_SOURCE_VALUE,
+  VALUE_SOURCE_VALUE
+from MEASUREMENT;" > data/eunomia/MEASUREMENT.csv
 
 # OBSERVATION
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  cast(OBSERVATION_ID as integer) as OBSERVATION_ID,
+  cast(PERSON_ID as integer) as PERSON_ID,
+  cast(OBSERVATION_CONCEPT_ID as integer) as OBSERVATION_CONCEPT_ID,
+  cast(OBSERVATION_DATE as integer) as OBSERVATION_DATE,
+  cast(OBSERVATION_DATETIME as integer) as OBSERVATION_DATETIME,
+  cast(OBSERVATION_TYPE_CONCEPT_ID as integer) as OBSERVATION_TYPE_CONCEPT_ID,
+  VALUE_AS_NUMBER,
+  VALUE_AS_STRING,
+  cast(VALUE_AS_CONCEPT_ID as integer) as VALUE_AS_CONCEPT_ID,
+  cast(QUALIFIER_CONCEPT_ID as integer) as QUALIFIER_CONCEPT_ID,
+  cast(UNIT_CONCEPT_ID as integer) as UNIT_CONCEPT_ID,
+  cast(PROVIDER_ID as integer) as PROVIDER_ID,
+  cast(VISIT_OCCURRENCE_ID as integer) as VISIT_OCCURRENCE_ID,
+  cast(VISIT_DETAIL_ID as integer) as VISIT_DETAIL_ID,
+  OBSERVATION_SOURCE_VALUE,
+  cast(OBSERVATION_SOURCE_CONCEPT_ID as integer) as OBSERVATION_SOURCE_CONCEPT_ID,
+  UNIT_SOURCE_VALUE,
+  QUALIFIER_SOURCE_VALUE
+from OBSERVATION;" > data/eunomia/OBSERVATION.csv
 
 # OBSERVATION_PERIOD
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  cast(OBSERVATION_PERIOD_ID as integer) as OBSERVATION_PERIOD_ID,
+  cast(PERSON_ID as integer) as PERSON_ID,
+  cast(OBSERVATION_PERIOD_START_DATE as integer) as OBSERVATION_PERIOD_START_DATE,
+  cast(OBSERVATION_PERIOD_END_DATE as integer) as OBSERVATION_PERIOD_END_DATE,
+  cast(PERIOD_TYPE_CONCEPT_ID as integer) as PERIOD_TYPE_CONCEPT_ID
+from OBSERVATION_PERIOD;" > data/eunomia/OBSERVATION_PERIOD.csv
 
 # PERSON
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  cast(PERSON_ID as integer) as PERSON_ID,
+  cast(GENDER_CONCEPT_ID as integer) as GENDER_CONCEPT_ID,
+  cast(YEAR_OF_BIRTH as integer) as YEAR_OF_BIRTH,
+  cast(MONTH_OF_BIRTH as integer) as MONTH_OF_BIRTH,
+  cast(DAY_OF_BIRTH as integer) as DAY_OF_BIRTH,
+  cast(BIRTH_DATETIME as integer) as BIRTH_DATETIME,
+  cast(RACE_CONCEPT_ID as integer) as RACE_CONCEPT_ID,
+  cast(ETHNICITY_CONCEPT_ID as integer) as ETHNICITY_CONCEPT_ID,
+  cast(LOCATION_ID as integer) as LOCATION_ID,
+  cast(PROVIDER_ID as integer) as PROVIDER_ID,
+  cast(CARE_SITE_ID as integer) as CARE_SITE_ID,
+  PERSON_SOURCE_VALUE,
+  GENDER_SOURCE_VALUE,
+  cast(GENDER_SOURCE_CONCEPT_ID as integer) as GENDER_SOURCE_CONCEPT_ID,
+  RACE_SOURCE_VALUE,
+  cast(RACE_SOURCE_CONCEPT_ID as integer) as RACE_SOURCE_CONCEPT_ID,
+  ETHNICITY_SOURCE_VALUE,
+  cast(ETHNICITY_SOURCE_CONCEPT_ID as integer) as ETHNICITY_SOURCE_CONCEPT_ID
+from PERSON;" > data/eunomia/PERSON.csv
 
 # PROCEDURE_OCCURENCE
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  cast(PROCEDURE_OCCURRENCE_ID as integer) as PROCEDURE_OCCURRENCE_ID,
+  cast(PERSON_ID as integer) as PERSON_ID,
+  cast(PROCEDURE_CONCEPT_ID as integer) as PROCEDURE_CONCEPT_ID,
+  cast(PROCEDURE_DATE as integer) as PROCEDURE_DATE,
+  cast(PROCEDURE_DATETIME as integer) as PROCEDURE_DATETIME,
+  cast(PROCEDURE_TYPE_CONCEPT_ID as integer) as PROCEDURE_TYPE_CONCEPT_ID,
+  cast(MODIFIER_CONCEPT_ID as integer) as MODIFIER_CONCEPT_ID,
+  cast(QUANTITY as integer) as QUANTITY,
+  cast(PROVIDER_ID as integer) as PROVIDER_ID,
+  cast(VISIT_OCCURRENCE_ID as integer) as VISIT_OCCURRENCE_ID,
+  cast(VISIT_DETAIL_ID as integer) as VISIT_DETAIL_ID,
+  PROCEDURE_SOURCE_VALUE,
+  cast(PROCEDURE_SOURCE_CONCEPT_ID as integer) as PROCEDURE_SOURCE_CONCEPT_ID,
+  MODIFIER_SOURCE_VALUE
+from PROCEDURE_OCCURRENCE;" > data/eunomia/PROCEDURE_OCCURRENCE.csv
 
 # RELATIONSHIP
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  RELATIONSHIP_ID,
+  RELATIONSHIP_NAME,
+  IS_HIERARCHICAL,
+  DEFINES_ANCESTRY,
+  REVERSE_RELATIONSHIP_ID,
+  cast(RELATIONSHIP_CONCEPT_ID as integer) as RELATIONSHIP_CONCEPT_ID
+from RELATIONSHIP;" > data/eunomia/RELATIONSHIP.csv
 
 # VISIT_OCCURENCE
 sqlite3 -header -csv cdm.sqlite "select
-
-from TABLE;" > data/eunomia/TABLE.csv
+  cast(VISIT_OCCURRENCE_ID as integer) as VISIT_OCCURRENCE_ID,
+  cast(PERSON_ID as integer) as PERSON_ID,
+  cast(VISIT_CONCEPT_ID as integer) as VISIT_CONCEPT_ID,
+  cast(VISIT_START_DATE as integer) as VISIT_START_DATE,
+  cast(VISIT_START_DATETIME as integer) as VISIT_START_DATETIME,
+  cast(VISIT_END_DATE as integer) as VISIT_END_DATE,
+  cast(VISIT_END_DATETIME as integer) as VISIT_END_DATETIME,
+  cast(VISIT_TYPE_CONCEPT_ID as integer) as VISIT_TYPE_CONCEPT_ID,
+  cast(PROVIDER_ID as integer) as PROVIDER_ID,
+  cast(CARE_SITE_ID as integer) as CARE_SITE_ID,
+  VISIT_SOURCE_VALUE,
+  cast(VISIT_SOURCE_CONCEPT_ID as integer) as VISIT_SOURCE_CONCEPT_ID,
+  cast(ADMITTING_SOURCE_CONCEPT_ID as integer) as ADMITTING_SOURCE_CONCEPT_ID,
+  ADMITTING_SOURCE_VALUE,
+  cast(DISCHARGE_TO_CONCEPT_ID as integer) as DISCHARGE_TO_CONCEPT_ID,
+  DISCHARGE_TO_SOURCE_VALUE,
+  cast(PRECEDING_VISIT_OCCURRENCE_ID as integer) as PRECEDING_VISIT_OCCURRENCE_ID
+from VISIT_OCCURRENCE;" > data/eunomia/VISIT_OCCURRENCE.csv
 
 # VOCABULARY
+sqlite3 -header -csv cdm.sqlite "select
+  VOCABULARY_ID,
+  VOCABULARY_NAME,
+  VOCABULARY_REFERENCE,
+  VOCABULARY_VERSION,
+  cast(VOCABULARY_CONCEPT_ID as integer) as VOCABULARY_CONCEPT_ID
+from VOCABULARY;" > data/eunomia/VOCABULARY.csv
 
+# Eunomia's vocabulary_version values in the VOCABULARY table has null values but the production schema do not allow that. Since the table only has empty values for those we can just find/replace ,, with ,"",
+sed -i.bak 's/,,/,"",/g' data/eunomia/VOCABULARY.csv
+rm data/eunomia/VOCABULARY.csv.bak # For mac portability, need ".bak"
 
 
